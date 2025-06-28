efe4658c96947490257c03538030e80f57012812
itsVaibhavSharma <itsvaibhavsharma007@gmail.com>
2025-06-28 22:00:01 +0530

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,37 @@
+// frontend/src/services/api.js
+import axios from 'axios'
+
+const API_BASE_URL = 'http://localhost:8000/api'
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Request interceptor to add auth token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('medix_token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// Response interceptor to handle token expiration
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('medix_token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
